f4fa9506eb4c49b7215a112de00a910640168ca2
toledoev <evelyndatoledo@gmail.com>
2023-04-04 02:16:10 +0100

    Merge remote-tracking branch 'origin/main'

--- README.md
+++ README.md
@@ -4,30 +4,52 @@ This is an online shop website where users can create an account and buy a range
 
 ## Getting Started
 
-1. Install the libraries from the file: `requirements.txt`
-2. In `main.py`, un-comment the funtions `databaseFunctions.create_database()` & `databaseFunctions.insertData()` to create the database and tables (Lines 409 & 410) 
-3. Re-comment the same lines again (Lines 409 & 410) 
-4. Run the file `main.py`
+1. Make sure to install all the required libraries from the file: `requirements.txt`
+2. In `main.py`, un-comment the funtions `databaseFunctions.create_database()` & `databaseFunctions.insertData()` `(Lines 444, 445, and 446) `
+& Run the file `main.py`
+3. Re-comment the same lines again `(Lines 444, 445, and 446)` 
+4. Run the file `main.py` again and go to the browser
 5. The landing page is the Provita Homepage
-6. Go to Signup page to create a new account OR go to Login page to login with the test user below:
+6. Go to `Signup` to create a new account then complete the Login. OR go to `Login` page to log in as the `admin` (Has access to the Admin page):
 
     - Username: `admin`
     - Password: `password`
-    - **Has access to Admin page**
+   
+7. Go to `My Profile` to display the current logged-in user's details
+8. Go to `My Orders` to display the current logged-in user's orders (if any)
+9. Go to `Shop` to search for products and add products to cart
+10. If the user choses to add products to cart, the user will be redirected to the `Checkout` page to complete the order.
+11. If the current logged-in user is the `admin`, go to `Admin` to add a new product to the database
+    - Product Name: mineralwater-watermelon-raspberry
+    - Upload an image: Chose the image `water-watermelon-raspberry` (make sure to save image provided in your computer)
+    - Set Price: 2.90
+    - And click on Submit
+    - Go to `Shop` and search for water to confirm the new product was added to the database
+    
+12. Go to `Logout` to log out from the website
+13. Go to `LinkedIn` to be redirected to the Social Media page ( There is no ProVita profile)
 
 ## Secure Web Programming
 
-1. Authentication - To prevent passwords from being reused in case of a data breach, the inputed password from the user is Hashed before been stored in the database.
+1. Authentication - To prevent passwords from being reused in case of a data breach, the password is `Hashed` before been stored in the database.
 
-2. Session Management - To prevent session cookies for being reused for session hajacking attacks, the library `session` adds the secret key to make the cookie stronger at the time of login. Another security measure, a logout option is provided to the user to terminate the session.
+2. Session Management - To prevent session cookies for being reused for session hijacking attacks, the library `session` adds the secret key to make the cookie stronger at the time of login. 
+Another security measure, a logout option is provided to the user to terminate the session.
 
-3. XSS - Scripts can be added by user input in the 'Signup' page. To prevent from XSS atacks, the 'Profile' HTML code is sanetized with regular expression that transforms javascript code into string so the script code won't run.<br>
+3. XSS - Scripts can be added by user input in the `Signup` page. 
+To prevent XSS attacks to display malicious scripts in HTML pages that read data from the database, the `Profile` HTML code is `encoded` which transforms JavasScript code into text, so the script code won't run.<br>
 
     **To test for XSS attack:**
     
-   1. Use the below credentials to login:<br>
+   1. Go to `Signup`, create a new account using the below code & then login:<br>
           - Username: `<script>alert(‘XSS’)</script>`<br>
           - Password: `<script>alert(‘XSS’)</script>` <br>
-   2. Go to the Profile page >> View Source Page >> Confirm the script is handled as string. 
+   2. Go to `My Profile` page >> View Source Page >> Confirm the script is handled as string. 
 
-4. SQL Injection - SQL injections can be added by user input in the 'Login' and 'Buy' pages. To prevent from SQL injection attacks, all queries to the database are parameterized and done with prepared statements. Therefore, incorrect parameters or arbitrary SQL code would not work to retrieve data from the database as the queries will return no results.
+4. SQL Injection - SQL injections can be added by user input in the `Login` and `Shop` pages (and by the admin input in the `Admin` page). 
+To prevent from SQL injection attacks, all queries to the database are `parameterized` and done with prepared statements. 
+Therefore, incorrect parameters or arbitrary SQL code would not work to retrieve data from the database as the queries will return no results.
+
+    ** To test for SQL Injection:**
+    1. Go to `Shop` and search for the following string: "water";DROP TABLE orders;
+    2. Logout and then go to `Login` and use the following string as username and passworkd: " or ""="
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 from datetime import date
-
 from flask import Flask, render_template, request, url_for, flash
 from werkzeug.utils import redirect, secure_filename
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_user, current_user, logout_user
-
 import databaseFunctions
 from user import User
 
+# Add name of database
 db_file = "mySQLite.db"
+# Path to image folders inside the project
 UPLOAD_FOLDER = 'static/images'
 
 app = Flask(__name__)
@@ -19,8 +19,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ENV'] = "Development"
 app.config['DEBUG'] = True
 
+# Create a Bcryp object for Password management
 bcrypt = Bcrypt(app)
 
+# Create a LoginManager object for login management
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'loginAction'
@@ -65,7 +67,7 @@ def signupAction():
         password = request.form.get("password")
 
         ## This password is a normal string
-        ## Therefore, we need to hash it before we put it in the database
+        ## Hashing the password before storing it in the database
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         # print("True Password:", password, ", Hashed Password:", hashed_password)
         password = hashed_password
@@ -86,21 +88,23 @@ def signupAction():
     try:
 
         cursor = conn.cursor()
+        # Query Paramiterisation
         sameUsernameCheck = "SELECT DISTINCT username FROM users WHERE username= ?"
         searchResult = cursor.execute(sameUsernameCheck, (username,))
+        # Checks if the username already exists
         usernameInDb = None
         for row in searchResult:
             usernameInDb = row[0]
         if usernameInDb:
-            flash('Username already exists.', category='error')
+            flash('Username already exists. Enter a different username', category='error')
             return redirect(url_for('signup'))
-
+        # Insert user to the database
+        # Query Paramiterisation
         myquery = "INSERT INTO users (username, password, email, first_name, last_name, age, activity) VALUES (?,?,?,?,?,?,?)"
         # print("My query is: ", myquery)
         cursor.execute(myquery, (username, password, email, first_name, last_name, age, activity))
         conn.commit()
-        ## if this works, this means the user have been added successfully
-        ## Therefore, we need to send them to the login page
+
     except Error as e:
         print(e)
         ## if the user is not added, we will get an exception which will be caught here
@@ -110,14 +114,15 @@ def signupAction():
     finally:
         if conn:
             conn.close()
+    ## if this works, this means the user have been added successfully
+    ## Therefore, we need to send them to the login page
     flash("Account created successfully!", category='success')
     return redirect(url_for('login'))
 
 
 @app.route('/login')
 def login():
-    ## @TODO
-    ## If user is already logged in we need to redirect them to their profile
+    ## If user is already logged in we need to redirect to the homepage
     if current_user.is_authenticated:
         flash("You are already logged in.", category='success')
         return redirect(url_for('home'))
@@ -137,6 +142,7 @@ def loginAction():
     try:
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
+        # Query Paramiterisation
         myquery = "SELECT password FROM users WHERE username= ?"
         data = cursor.execute(myquery, (username,))
         passwordInDB = None
@@ -156,7 +162,7 @@ def loginAction():
             else:
                 ## The password does not match
                 ## we need to send them to the login page again
-                flash("Your username or password are incorrect! Try to login again.", category='error')
+                flash("Your password is incorrect! Try to login again.", category='error')
                 return redirect(url_for('login'))
                 pass
         else:
@@ -168,7 +174,7 @@ def loginAction():
         print(e)
         ## if there was an error in the login, we will get an exception which will be caught here
         ## So we need to send the user to the login page again
-        flash("Failled to login. Try again!", category='error')
+        flash("Failed to login. Try again!", category='error')
         return redirect(url_for('login'))
     finally:
         if conn:
@@ -182,7 +188,7 @@ def profile():
         flash("You need to login to access the profile page.", category='error')
         return redirect(url_for('login'))
 
-    ## If the user is logged in, we need to get their username
+    ## If the user is logged in, we need to get their details
     myusername = current_user.username
     myemail = ""
     myfname = ""
@@ -195,7 +201,8 @@ def profile():
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
 
-        # Get the personal data of the user
+        # Read the data from the database of the user current logged in
+        # Query Paramiterisation
         myquery = "SELECT email, first_name, last_name, age, activity FROM users WHERE username=?"
         dataUser = cursor.execute(myquery, (myusername,))
         print("These are the details of the user: ")
@@ -218,13 +225,14 @@ def profile():
     finally:
         if conn:
             conn.close()
-    # Render the user details
+    # Render the user details. The profile.html page the output is encoded to prevent XSS attacks.
     return render_template("profile.html", username=myusername, email=myemail, first_name=myfname, last_name=mylname,
                            age=myage, activity=myactivity)
 
 
 @app.route('/orders', methods=['GET'])
 def orders():
+    ## If the user is not logged in, we need to redirect them to the login page
     if not current_user.is_authenticated:
         flash("You need to login to access the orders page.", category='error')
         return redirect(url_for('login'))
@@ -236,12 +244,13 @@ def orders():
         cursor = conn.cursor()
 
         # Get the orders of the User
+        # Query Paramiterisation
         myquery = "SELECT order_date, orders.product_name, quantity, orders.image_path, price FROM orders join users on orders.username = users.username left join products on orders.product_name = products.product_name WHERE orders.username= ? ORDER BY order_date"
         cursor.execute(myquery, (current_user.username,))
         # print("These are the user's orders: ")
         rows = cursor.fetchall()
 
-        # Render the orders details
+        # Render the orders details in the orders.html page
         return render_template("orders.html", ordersData=rows)
     except Error as e:
         print(e)
@@ -252,27 +261,36 @@ def orders():
 
 @app.route('/search')
 def search():
+    ## If the user is not logged in, we need to redirect them to the login page
+    if not current_user.is_authenticated:
+        flash("You need to login to access the orders page.", category='error')
+        return redirect(url_for('login'))
+    # Render the search.html page
     return render_template("search.html")
 
 
 @app.route('/searchAction')
 def searchAction():
+    ## If the user is not logged in, we need to redirect them to the login page
     if not current_user.is_authenticated:
         flash("You need to login to access the profile page.", category='error')
         return redirect(url_for('login'))
 
+    # Get user's search input
     searchString = request.args.get("searchString")
     conn = None
     try:
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
         likeString = f"%{searchString}%"
+        # Query Paramiterisation
+        # Check if the search input exists in the database
         cursor.execute("SELECT product_id, product_name, image_path, price FROM products WHERE product_name LIKE ?",
                        (likeString,))
         data = cursor.fetchall()
         print("These are the products found: ", data)
-
-        return render_template('search.html', products=data)
+        # Render the query results in the search.html page
+        return render_template('search.html', products=data, searchInput=searchString)
 
     except Error as e:
         print(e)
@@ -283,6 +301,12 @@ def searchAction():
 
 @app.route('/addToCart', methods=['POST'])
 def addToCart():
+    ## If the user is not logged in, we need to redirect them to the login page
+    if not current_user.is_authenticated:
+        flash("You need to login to access the orders page.", category='error')
+        return redirect(url_for('login'))
+    # Get the data from the selected row
+    # Send the data to checkout.html page
     product_name = request.form['product_name']
     image = request.form['image']
     quantity = int(request.form['quantity'])
@@ -292,6 +316,11 @@ def addToCart():
 
 @app.route('/checkout', methods=['GET'])
 def checkout():
+    ## If the user is not logged in, we need to redirect them to the login page
+    if not current_user.is_authenticated:
+        flash("You need to login to access the orders page.", category='error')
+        return redirect(url_for('login'))
+    # Show the data in the checkout.html page
     product_name = request.args.get('product_name')
     image = request.args.get('image')
     quantity = request.args.get('quantity')
@@ -301,7 +330,12 @@ def checkout():
 
 @app.route('/completeOrder', methods=['POST'])
 def completeOrder():
-    # Insert the new order into the table 'orders' in the database
+    ## If the user is not logged in, we need to redirect them to the login page
+    if not current_user.is_authenticated:
+        flash("You need to login to access the orders page.", category='error')
+        return redirect(url_for('login'))
+
+    # Insert the selected row data into the 'orders' table in the database
     product_name = request.form.get('product_name')
     image_path = request.form['image']
     quantity = int(request.form['quantity'])
@@ -309,20 +343,16 @@ def completeOrder():
     order_date = date.today()
     username = current_user.username
 
-    # print(product_name)
-    # print(image_path)
-    # print(quantity)
-    # print(price)
-
     conn = sqlite3.connect(db_file)
 
     try:
         cursor = conn.cursor()
-        # print(product_name, image_path, quantity, order_date, username)
+        # Query Paramiterisation
         myquery = "INSERT INTO orders ( product_name, image_path, quantity, order_date, username) VALUES (?,?,?,?,?)"
         # print("My query is: ", myquery)
         cursor.execute(myquery, (product_name, image_path, quantity, order_date, username))
         conn.commit()
+
     except Error as e:
         print(e)
         flash("Error. Order failed. ", category='error')
@@ -335,9 +365,11 @@ def completeOrder():
 
 @app.route('/admin')
 def admin():
+    ## If the user is not logged in, we need to redirect them to the login page
     if not current_user.is_authenticated:
         flash("You need to login to access the profile page.", category='error')
         return redirect(url_for('login'))
+    ## If the user logged in is not the 'admin', the user cannot access this page
     if current_user.username == 'admin':
         return render_template("admin.html")
     else:
@@ -346,7 +378,7 @@ def admin():
 
 @app.route('/addProduct', methods=['POST'])
 def addProduct():
-    # Only the below files types are accepted
+    # Only the below files types are accepted to be uploaded
     ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
     def allowed_file(filename):
@@ -356,18 +388,18 @@ def addProduct():
     # Upload the image to the project folder
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file part.')
             return redirect(request.url)
         file = request.files['file']
         if file and allowed_file(file.filename):
             image_path = os.path.join(os.path.join(app.config['UPLOAD_FOLDER']), secure_filename(file.filename))
             file.save(image_path)
-            print("Image path" + image_path)
-            flash("File uploaded successfully", category='success')
+            print("Image path: " + image_path)
+            flash("File uploaded successfully.", category='success')
 
     # Insert the new product into the table products in the database
     product_name = ""
-    image_path = "image_path"
+    image_path = image_path
     price = ""
 
     if request.form.get("product_name"):
@@ -381,13 +413,19 @@ def addProduct():
 
     try:
         cursor = conn.cursor()
+        # Query Paramiterisation. Admin input is still a user input that needs to be secured.
         myquery = "INSERT INTO products (product_name, image_path, price) VALUES (?,?,?)"
         print("New product added to database: ", myquery)
         cursor.execute(myquery, (product_name, image_path, price))
         conn.commit()
+
+        print(product_name)
+        print(image_path)
+        print(price)
+
     except Error as e:
         print(e)
-        flash("Error. Product not added. ", category='error')
+        flash("Error. Product not added. Try again. ", category='error')
     finally:
         if conn:
             conn.close()
@@ -397,13 +435,14 @@ def addProduct():
 
 @app.route('/logout')
 def logout():
+    ## If the user is log in, then logout the user
     if current_user.is_authenticated:
         logout_user()
     return render_template("logout.html")
 
 
 if __name__ == '__main__':
-    # databaseFunctions.create_database()  # Creates "users", "products" and "orders" tables
+    # databaseFunctions.create_database()  # Creates the "mySQLite" database & the "users", "products" and "orders" tables
     # databaseFunctions.insertData()  # Inserts data into "users" and "products" tables
     # databaseFunctions.displayData()  # Displays orders data
     app.run(host='127.0.0.1', port=5000, debug=True)
